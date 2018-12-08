3239046b479ff062aa8f29a10d6884341a6a8ab4
eric <heathornhawk@gmail.com>
2018-12-07 16:19:50 -0800

    updates

--- src/app/pages/editor/EditorPage.tsx
+++ src/app/pages/editor/EditorPage.tsx
@@ -1,42 +1,45 @@
 import * as React from "react";
+
+import { IPageProps, IPageState, Page } from "@app.pages";
 import { Icon, IconSource } from "@core.icons";
+
 import { SVG } from "@core.components";
-import { Page, IPageProps, IPageState } from "@app.pages";
 
 interface IEditorProps extends IPageProps {}
 
 interface IEditorState extends IPageState {}
 
 export class EditorPage extends Page<IEditorProps, IEditorState> {
-    private readonly _svgRef: React.RefObject<SVG>;
+	private readonly _svgRef: React.RefObject<SVG>;
 
-    public constructor(props: IEditorProps) {
-        super(props);
-        this._svgRef = React.createRef();
-    }
+	public constructor(props: IEditorProps) {
+		super(props);
+		this._svgRef = React.createRef();
+	}
 
-    public componentDidMount() {
-        super.componentDidMount();
-        const svg: SVG = this._svgRef.current as SVG;
+	public componentDidMount() {
+		super.componentDidMount();
 
-        setTimeout(() => {
-            svg.fill = "blue";
-        }, 3000);
-    }
+		const svg: SVG = this._svgRef.current as SVG;
 
-    public render() {
-        return (
-            <div style={{ color: this.theme.colors.font.highMedium }} className="editor">
-                <div>Hello</div>
+		setTimeout(() => {
+			svg.fill = "blue";
+		}, 3000);
+	}
 
-                <div style={{ height: 200 }}>
-                    <SVG ref={this._svgRef}>
-                        <rect shapeRendering="crispEdges" width="100%" height="10%" y="10%" />
-                        <rect shapeRendering="crispEdges" width="100%" height="10%" y="45%" />
-                        <rect shapeRendering="crispEdges" width="100%" height="10%" y="80%" />
-                    </SVG>
-                </div>
-            </div>
-        );
-    }
+	public render() {
+		return (
+			<div style={{ color: this.theme.colors.font.highMedium }} className="editor">
+				<div>Hello</div>
+
+				<div style={{ width: 50 }}>
+					<SVG ref={this._svgRef}>
+						<rect shapeRendering="crispEdges" width="100%" height="10%" y="10%" />
+						<rect shapeRendering="crispEdges" width="100%" height="10%" y="45%" />
+						<rect shapeRendering="crispEdges" width="100%" height="10%" y="80%" />
+					</SVG>
+				</div>
+			</div>
+		);
+	}
 }
--- src/core/components/svg/SVGBase.tsx
+++ src/core/components/svg/SVGBase.tsx
@@ -1,246 +1,260 @@
 import * as React from "react";
 
-import { IVector2, IBounds, IDegree, ISize } from "@core.interfaces";
 import { Control, IControlProps, IControlState } from "@core.components";
+import { IBounds, IDegree, ISize, IVector2 } from "@core.interfaces";
+
 import { VectorHelpers } from "@core.utils";
 
 export interface ISVGProps extends IControlProps {
-    fill: string;
-    width: number;
-    height: number;
-    translate: IVector2;
-    scale: IVector2;
-    rotate: IDegree;
-    preserveAspect: boolean;
-    stroke: string;
-    strokeWidth: number;
-    clip: boolean;
+	fill: string;
+	width: number;
+	height: number;
+	translate: IVector2;
+	scale: IVector2;
+	rotate: IDegree;
+	preserveAspect: boolean;
+	stroke: string;
+	strokeWidth: number;
+	clip: boolean;
 }
 
 export interface ISVGState extends IControlState {
-    fill: string;
-    width: number;
-    height: number;
-    translate: IVector2;
-    scale: IVector2;
-    rotate: IDegree;
-    stroke: string;
-    strokeWidth: number;
-    clip: boolean;
+	fill: string;
+	width: number;
+	height: number;
+	translate: IVector2;
+	scale: IVector2;
+	rotate: IDegree;
+	stroke: string;
+	strokeWidth: number;
+	clip: boolean;
 }
 
 export abstract class SVGBase<TProps extends ISVGProps, TState extends ISVGState> extends Control<TProps, TState> {
-    public static defaultProps: ISVGProps = {
-        fill: "red",
-        width: 0,
-        height: 0,
-        translate: { x: 0, y: 0 },
-        scale: { x: 1, y: 1 },
-        rotate: { deg: 0 },
-        preserveAspect: true,
-        stroke: "green",
-        strokeWidth: 3,
-        clip: true
-    };
+	public static defaultProps: ISVGProps = {
+		fill: "red",
+		width: 0,
+		height: 0,
+		translate: { x: 0, y: 0 },
+		scale: { x: 1, y: 1 },
+		rotate: { deg: 0 },
+		preserveAspect: true,
+		stroke: "green",
+		strokeWidth: 3,
+		clip: true
+	};
 
-    private _isAutoSizeX: boolean = false;
-    private _isAutoSizeY: boolean = false;
+	private _isAutoSizeX: boolean = false;
+	private _isAutoSizeY: boolean = false;
 
-    private readonly _containerRef: React.RefObject<HTMLDivElement>;
-    private readonly _svgRef: React.RefObject<SVGSVGElement>;
-    private _calculated: boolean;
+	private readonly _containerRef: React.RefObject<HTMLDivElement>;
+	private readonly _svgRef: React.RefObject<SVGSVGElement>;
+	private _calculated: boolean;
 
-    public constructor(props: TProps) {
-        super(props);
+	public constructor(props: TProps) {
+		super(props);
 
-        if (this.props.width <= 0) {
-            this._isAutoSizeX = true;
-        }
+		if (this.props.width <= 0) {
+			this._isAutoSizeX = true;
+		}
 
-        if (this.props.height <= 0) {
-            this._isAutoSizeY = true;
-        }
+		if (this.props.height <= 0) {
+			this._isAutoSizeY = true;
+		}
 
-        this._containerRef = React.createRef();
-        this._svgRef = React.createRef();
-    }
+		this._containerRef = React.createRef();
+		this._svgRef = React.createRef();
+	}
 
-    //preserveAspectRatio="xMidYMid meet"
-    public componentWillMount() {
-        super.componentDidMount();
+	//preserveAspectRatio="xMidYMid meet"
+	public componentWillMount() {
+		super.componentDidMount();
+		this.setState({
+			fill: this.props.fill,
+			width: this.props.width,
+			height: this.props.height,
+			translate: this.props.translate,
+			scale: this.props.scale,
+			rotate: this.props.rotate
+		});
+	}
 
-        this.setState({
-            fill: this.props.fill,
-            width: this.props.width,
-            height: this.props.height,
-            translate: this.props.translate,
-            scale: this.props.scale,
-            rotate: this.props.rotate
-        });
-    }
+	protected calculateSize(): ISize {
+		let width: number = this.props.width;
+		let height: number = this.props.height;
 
-    protected calculateSize(): ISize {
-        let width: number = this.props.width;
-        let height: number = this.props.height;
+		if (this.containerElement.parentElement) {
+			const parent = this.containerElement.parentElement;
+			if (this._isAutoSizeX) {
+				width = parent.offsetWidth;
+			}
 
-        if (this.containerElement.parentElement) {
-            const parent = this.containerElement.parentElement;
+			if (this._isAutoSizeY) {
+				height = parent.offsetHeight;
+			}
 
-            if (this._isAutoSizeX) {
-                width = parent.offsetWidth;
-            }
-            if (this._isAutoSizeY) {
-                height = parent.offsetHeight;
-            }
+			if (this.props.preserveAspect) {
+				let min = Math.min(width, height);
 
-            if (this.props.preserveAspect) {
-                const min = Math.min(width, height);
-                width = min;
-                height = min;
-            }
-        }
+				if (width === 0 || height === 0) {
+					min = Math.max(width, height);
+				}
 
-        return { width: width, height: height };
-    }
+				width = min;
+				height = min;
+			}
+		}
 
-    public componentDidMount() {
-        super.componentDidMount();
+		return { width: width, height: height };
+	}
 
-        const size: ISize = this.calculateSize();
+	public componentDidMount() {
+		super.componentDidMount();
 
-        this.width = size.width;
-        this.height = size.height;
-    }
+		const size: ISize = this.calculateSize();
 
-    public get svgElement(): SVGSVGElement {
-        return this._svgRef.current as SVGSVGElement;
-    }
+		this.width = size.width;
+		this.height = size.height;
+	}
 
-    public get containerElement(): HTMLDivElement {
-        return this._containerRef.current as HTMLDivElement;
-    }
+	public get svgElement(): SVGSVGElement {
+		return this._svgRef.current as SVGSVGElement;
+	}
 
-    public get fill(): string {
-        return this.state.fill;
-    }
+	public get containerElement(): HTMLDivElement {
+		return this._containerRef.current as HTMLDivElement;
+	}
 
-    public set fill(value: string) {
-        if (this.state.fill !== value) {
-            this.setState({ fill: value });
-        }
-    }
+	public get fill(): string {
+		return this.state.fill;
+	}
 
-    public get width(): number {
-        return this.state.width;
-    }
+	public set fill(value: string) {
+		if (this.state.fill !== value) {
+			this.setState({ fill: value });
+		}
+	}
 
-    public set width(value: number) {
-        if (this.state.width !== value) {
-            this.setState({ width: value });
-        }
-    }
+	public get width(): number {
+		return this.state.width;
+	}
 
-    public get height(): number {
-        return this.state.width;
-    }
+	public set width(value: number) {
+		if (this.state.width !== value) {
+			this.setState({ width: value });
+		}
+	}
 
-    public set height(value: number) {
-        if (this.state.height !== value) {
-            this.setState({ height: value });
-        }
-    }
+	public get height(): number {
+		return this.state.width;
+	}
 
-    public get translate(): IVector2 {
-        return this.state.translate;
-    }
+	public set height(value: number) {
+		if (this.state.height !== value) {
+			this.setState({ height: value });
+		}
+	}
 
-    public set translate(value: IVector2) {
-        if (!VectorHelpers.isVector2Equal(this.state.translate, value)) {
-            this.setState({ translate: value });
-        }
-    }
+	public get translate(): IVector2 {
+		return this.state.translate;
+	}
 
-    public get rotate(): IDegree {
-        return this.state.rotate;
-    }
+	public set translate(value: IVector2) {
+		if (!VectorHelpers.isVector2Equal(this.state.translate, value)) {
+			this.setState({ translate: value });
+		}
+	}
 
-    public set rotate(value: IDegree) {
-        if (this.state.rotate.deg !== value.deg) {
-            this.setState({ rotate: value });
-        }
-    }
+	public get rotate(): IDegree {
+		return this.state.rotate;
+	}
 
-    public get scale(): IVector2 {
-        return this.state.scale;
-    }
+	public set rotate(value: IDegree) {
+		if (this.state.rotate.deg !== value.deg) {
+			this.setState({ rotate: value });
+		}
+	}
 
-    public set scale(value: IVector2) {
-        if (!VectorHelpers.isVector2Equal(this.state.scale, value)) {
-            this.setState({ scale: value });
-        }
-    }
+	public get scale(): IVector2 {
+		return this.state.scale;
+	}
 
-    public get viewBox(): IBounds {
-        return { x: this.translate.x, y: this.translate.y, width: this.width, height: this.height };
-    }
+	public set scale(value: IVector2) {
+		if (!VectorHelpers.isVector2Equal(this.state.scale, value)) {
+			this.setState({ scale: value });
+		}
+	}
 
-    public render() {
-        return (
-            <div ref={this._containerRef} className="svg-container" style={{ width: this.width, height: this.height }}>
-                <svg
-                    style={{ fillRule: "evenodd" }}
-                    ref={this._svgRef}
-                    className="svg-image"
-                    fill={this.fill}
-                    viewBox={`${this.translate.x} ${this.translate.y} ${this.width} ${this.height}`}
-                    preserveAspectRatio={this.preserveAspect()}
-                >
-                    {this.clipPath()}
-                    <g clipPath="url(#clipper)" transform={this.transformString()}>
-                        {this.strokeRect()}
-                        {this.props.children}
-                    </g>
-                </svg>
-            </div>
-        );
-    }
+	public get viewBox(): IBounds {
+		return { x: this.translate.x, y: this.translate.y, width: this.width, height: this.height };
+	}
 
-    protected clipPath(): JSX.Element | null {
-        if (this.props.clip) {
-            return (
-                <clipPath id="clipper">
-                    <rect width={this.width} height={this.height} x={this.translate.x} y={this.translate.y} />
-                </clipPath>
-            );
-        }
+	public render() {
+		console.log(this.width, this.state.width);
+		return (
+			<div ref={this._containerRef} className="svg-container" style={{ width: this.width, height: this.height }}>
+				<svg
+					ref={this._svgRef}
+					className="svg-image"
+					fill={this.fill}
+					style={{ width: this.width, height: this.height, fillRule: "evenodd" }}
+					viewBox={`${this.translate.x} ${this.translate.y} ${this.width} ${this.height}`}
+					preserveAspectRatio={this.preserveAspect()}
+				>
+					{this.clipPath()}
+					<g clipPath="url(#clipper)" transform={this.transformString()}>
+						{this.props.children}
+						{this.strokeRect()}
+					</g>
+				</svg>
+			</div>
+		);
+	}
 
-        return null;
-    }
+	protected clipPath(): JSX.Element | null {
+		if (this.props.clip) {
+			return (
+				<clipPath id="clipper">
+					<rect width={this.width} height={this.height} x={this.translate.x} y={this.translate.y} />
+				</clipPath>
+			);
+		}
 
-    protected strokeRect(): JSX.Element | null {
-        if (this.props.strokeWidth > 0) {
-            const strokeOffset = this.props.strokeWidth * 2;
-            const width = this.width - strokeOffset;
-            const height = this.height - strokeOffset;
+		return null;
+	}
 
-            return <rect stroke={this.props.stroke} strokeWidth={this.props.strokeWidth} width={width} height={height} x={this.translate.x} y={this.translate.y} />;
-        }
+	protected strokeRect(): JSX.Element | null {
+		if (this.props.strokeWidth > 0) {
+			return (
+				<rect
+					stroke={this.props.stroke}
+					strokeWidth={this.props.strokeWidth}
+					fill="transparent"
+					width={this.width}
+					height={this.height}
+					x={this.translate.x}
+					y={this.translate.y}
+				/>
+			);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    protected preserveAspect(): string {
-        let str = "";
+	protected preserveAspect(): string {
+		let str = "";
 
-        if (this.props.preserveAspect) {
-            //str += "xMidYMid meet";
-            str += "none";
-        }
+		if (this.props.preserveAspect) {
+			//str += "xMidYMid meet";
+			str += "none";
+		}
 
-        return str;
-    }
+		return str;
+	}
 
-    protected transformString(): string {
-        return `rotate(${this.rotate.deg}) translate(${this.translate.x} ${this.translate.y}) scale(${this.scale.x} ${this.scale.y})`;
-    }
+	protected transformString(): string {
+		return `rotate(${this.rotate.deg}) translate(${this.translate.x} ${this.translate.y}) scale(${this.scale.x} ${
+			this.scale.y
+		})`;
+	}
 }
